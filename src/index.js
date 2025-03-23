f661f3e6a936c595719bd1ba1fd4c601cb6aa0cd
collins muriuki <muriukicollins48@gmail.com>
2025-03-23 10:42:14 +0300

    Fetching code for displaying characters

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-// Your code here
+//fetching charactersto be displayed
+fetch('http://localhost:3000/characters')
+.then(response => response.json())
+.then(characters => {
+    const charBar = document.getElementById('character-bar');
+    characters.forEach(character => {
+        const span = document.createElement('span');
+        span.textContent = character.name;
+        charBar.appendChild(span);
+         span.addEventListener('click', () => showCharacterDetails(character.id));
+
+    });
+})
+.catch(error => {
+    console.error('Fetching characters not succcessful', error)
+});
